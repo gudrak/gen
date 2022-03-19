aee87efa6cc0176570fbd1065910a3ae81d2898c
gudrak <63885769+zionserver@users.noreply.github.com>
2022-03-19 01:12:29 -0300

    Update and rename README.md to gen.sh

---
+++ gen.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+
+# Additional packages in the ISO
+PKGS='bzip2 cryptsetup curl ddrescue gnupg2 git gptfdisk gzip efibootmgr lvm2 makepasswd mdadm mosh nano p7zip rsync wget vim unzip xz zip'
+
+# Base packages required for livecd
+# Source: https://github.com/void-linux/void-mklive/blob/master/build-x86-images.sh.in
+BASE_PKGS='dialog cryptsetup lvm2 mdadm void-docs-browse grub-i386-efi grub-x86_64-efi'
+
+# List of mirrors (see also: ci/set_repository.sh):
+#   https://docs.voidlinux.org/xbps/repositories/mirrors/
+REPO='https://mirrors.dotsrc.org/voidlinux/current'
+
+# Set repository
+rm -fr /etc/xbps.d
+mkdir -p -m 755 /etc/xbps.d
+echo "repository=${REPO}" > /etc/xbps.d/repo.conf
+
+xbps-install --yes -Su xbps
+xbps-install --yes -Su
+xbps-install --yes -S git make
+cd '/root'
+[ ! -d 'void-mklive' ] && git clone -b master --single-branch --depth 1 'https://github.com/void-linux/void-mklive.git' 'void-mklive'
+cd 'void-mklive/'
+make clean
+make
+./mklive.sh -a 'x86_64' -r "${REPO}" -p "${BASE_PKGS} ${PKGS}"
